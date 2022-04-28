d97d7fe4c96079527ef7430252fc98298a4f9dfe
chaerin <achrvv@sookmyung.ac.kr>
2022-04-28 15:20:55 +0900

    Initial setting

---
+++ gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `Chaerin00.log`,
+  description: `배운 것들 안 까먹으려고 쓰는 블로그 📕`,
+  author: `Chaerin`,
+  introduction: `배운 것들 안 까먹으려고 쓰는 블로그 📕`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `chaerin00`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: `chaerin00`, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    // TODO: instaill utterances
+    utterances: 'chaerin00/react-dynamic-theme-web', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}
---
+++ src/components/contents/index.jsx
@@ -0,0 +1,29 @@
+import React, { useMemo, useEffect } from 'react'
+
+import { ThumbnailContainer } from '../thumbnail-container'
+import { ThumbnailItem } from '../thumbnail-item'
+import { CATEGORY_TYPE } from '../../constants'
+
+export const Contents = ({ posts, countOfInitialPost, count, category }) => {
+  const refinedPosts = useMemo(() =>
+    posts
+      .filter(
+        ({ node }) =>
+          category === CATEGORY_TYPE.ALL ||
+          node.frontmatter.category === category
+      )
+      .slice(0, count * countOfInitialPost)
+  )
+
+  useEffect(() => {
+    console.log(refinedPosts)
+  }, [])
+
+  return (
+    <ThumbnailContainer>
+      {refinedPosts.map(({ node }, index) => (
+        <ThumbnailItem node={node} key={`item_${index}`} />
+      ))}
+    </ThumbnailContainer>
+  )
+}
